bf649ee729ebe72569cdee7790e0e4b5a36cdd74
Peyton Kellogg <pekellogg@hotmail.com>
2023-01-14 19:31:28 -0800

    commit tree 5af7f90eff6c422a64201c822db2464d614990f5

--- src/App.js
+++ src/App.js
@@ -1,1 +1,40 @@
-// create your App component here
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  state = {
+    peopleInSpace: [],
+  };
+
+  componentDidMount() {
+    fetch("http://api.open-notify.org/astros.json")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          peopleInSpace: data.people,
+        });
+    });
+  };
+
+  displaySpacePeople() {
+    return this.state.peopleInSpace.map((person) => {
+      return (
+        <div key={person.name}>
+          {person.name}
+        </div>
+      );
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        {/* {this.state.peopleInSpace.map((person) => person.name)} */}
+        {this.displaySpacePeople()}
+      </div>
+    );
+  };
+
+};
+
+export default App;
